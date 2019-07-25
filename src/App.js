2f49e7b7d204b279d0e5bb18573b7265728c046f
Matthew Boote <bootematt1@gmail.com>
2019-07-25 22:47:56 +1200

    added update of state for searching.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import React , {Component} from 'react';
-import {CardList} from './components/card-list/card-list.component'
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import "./App.css";
 
-class App extends Component{
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
 
     this.state = {
-     monsters : []
+      monsters: [],
+      searchField: ""
     };
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters : users}));
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users =>
+        this.setState({
+          monsters: users
+        })
+      );
   }
 
-
-  render(){
+  render() {
     return (
       <div className="App">
-        <CardList monsters = {this.state.monsters}/>
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={e => {
+            this.setState({
+              searchField: e.target.value
+            });
+          }}
+        />
+
+        <CardList monsters={this.state.monsters} />
       </div>
-    )
+    );
   }
 }
 
