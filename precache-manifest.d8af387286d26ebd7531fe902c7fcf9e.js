b79f627e28f5dc4892c126d8fb33a625086de143
Matthew Boote <bootematt1@gmail.com>
2019-07-26 19:34:34 +1200

    Updates

---
+++ precache-manifest.d8af387286d26ebd7531fe902c7fcf9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856c5134fb69f9f38a0cfad25443fa9e",
+    "url": "/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b781efcb210bcf7646f5",
+    "url": "/github.io/monsters-rolodex/static/css/main.7fe2f969.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/github.io/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b781efcb210bcf7646f5",
+    "url": "/github.io/monsters-rolodex/static/js/main.344c6733.chunk.js"
+  },
+  {
+    "revision": "1ac109da61bf9b161c11",
+    "url": "/github.io/monsters-rolodex/static/js/runtime~main.decfa424.js"
+  }
+]);
